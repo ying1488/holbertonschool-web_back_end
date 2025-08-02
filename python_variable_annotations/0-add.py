3205ec7d2200652236005abbb11f2b49e5b21611
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-08-02 08:57:43 +0000

    task 0

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,9 +1,22 @@
 #!/usr/bin/env python3
+"""
+Module
+"""
+
 def add(a: float, b: float) -> float:
+    """
+    Add two floting-point numbers.
+    
+    Args: 
+    a(float): the first number.
+    b(float): the second number
+
+    Returns: 
+    floats: The sum of a and b. 
+    """
     if not isinstance(a, float):
             raise TypeError("a must be float")
     if not isinstance(b, float):
             raise TypeError("b must be float")
-    
     return a + b
     
