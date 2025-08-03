96fbff23ea4d9fb7601560e26c7d82bba75c0d00
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-08-03 05:02:08 +0000

    task 7

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -6,7 +6,7 @@ Complex types - string and int/float to tuple
 from typing import Union, List, Tuple
 
 
-def to_kv(k:str, v:Union[int,float]) -> Tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
     function that returns sum as a float.
 
