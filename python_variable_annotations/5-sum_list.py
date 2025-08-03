dbcadb7700814b108b9cf479807bdb9807ea434b
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-08-03 04:23:05 +0000

    task 5

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -5,13 +5,13 @@ Complex types - list of floats
 
 from typing import List
 
-def sum_list (input_list: List[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """
     which takes a list of floats as argument and returns their sum as a float.
 
 
     Args:
-        input_list: list 
+        input_list: list
 
 
     Returns:
