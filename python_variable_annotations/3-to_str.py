be7bb23871c2bfb677b8b576935db915081af2a6
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-08-03 03:57:04 +0000

    task 3

--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -4,6 +4,7 @@ Floor
 """
 import math
 
+
 def to_str(n: float) -> str:
     """
     Converts a float to its string representation.
@@ -15,7 +16,6 @@ def to_str(n: float) -> str:
 
     Returns:
         The string representation of the float.
-
     """
-    
+
     return str(n)
