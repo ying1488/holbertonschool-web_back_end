5c3d1e6fbae032a9186c2e2fc9601aa463dd5296
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-08-03 04:20:51 +0000

    tasl 5

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Complex types - list of floats
+"""
+
+from typing import List
+
+def sum_list (input_list: list) -> float:
+    """
+    which takes a list of floats as argument and returns their sum as a float.
+
+
+    Args:
+        input_list: list 
+
+
+    Returns:
+        The sum as a float
+    """
+
+    return float(sum(input_list))
