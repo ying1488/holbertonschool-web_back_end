05f308e96e247f42cce74cf30f3048b9befd3158
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-08-04 01:52:14 +0000

    task *

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -2,9 +2,9 @@
 
 """module"""
 
-
 import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
+
 
 def task_wait_random(max_delay: int = 10) -> asyncio.Task:
     """function that takes an integer max_delay"""
