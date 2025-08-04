b2d1d8443f594b80a801354115bcc929263646b8
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-08-04 03:15:49 +0000

    task 0

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import asyncio
+import random
+from typing import Generator
+
+async def async_generator() -> Generator[float, None, None]:
+    """
+    Function
+    """
+
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
