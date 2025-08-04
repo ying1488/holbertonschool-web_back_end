1c2ddbddaed086e2080df30016091e06e3718db6
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-08-04 03:38:40 +0000

    task 1

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -3,7 +3,7 @@
 
 import asyncio
 import time
-async_comprehension =__import__('1-async_comprehension').async_comprehension
+async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
