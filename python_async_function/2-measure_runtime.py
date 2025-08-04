16954ea63fb8f14d79b6103b4ac27c59f582d9d7
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-08-04 01:38:23 +0000

    task *

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -2,7 +2,7 @@
 """module"""
 import asyncio
 import time
-wait_n = __import__('1-concurrent_coroutines.py').wait_n
+wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
 def measure_time(n: int, max_delay: int = 10) -> float:
