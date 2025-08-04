a1505ef172fb37c70e399288ab6aecaf9d680ee7
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-08-04 01:08:04 +0000

    task *

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -12,10 +12,10 @@ async def wait_n(n: int, max_delay: int = 10) -> List[float]:
     for _ in range(n):
         task = asyncio.create_task(wait_random(max_delay))
         task_list.append(task)
-    
+
     sorted_list = []
     for task in asyncio.as_completed(task_list):
         result = await task
         sorted_list.append(result)
-    
+
     return sorted_list
