7b97e390427cc2d4818073dc0646bf00ee49a336
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-08-04 03:24:57 +0000

    task 0

---
+++ python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" Module """
+
+from typing import List 
+async_generator = __import__('0-async_generator').async_generator
+
+async def async_comprehension() -> List[float]:
+    """
+    function
+    """
+    results = [i async for i in async_generator()]
+    return results
