c9714fb7f6bf3050a6795ca1795b419f68f9eb41
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-08-28 07:39:24 +0000

    task 0

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -8,6 +8,9 @@ for a specific page in pagination.
 from typing import Tuple
 
 def index_range(page: int, page_size: int) -> Tuple:
+    """ This function calculates the start index and end index
+    and returns a tuple of the results
+    """
     start_idx = (page - 1) * page_size
     end_idx = page * page_size
     return (start_idx, end_idx)
