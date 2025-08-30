bddce8ab5a6aac07657a60277bc0c056f76a895b
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-08-30 07:51:16 +0000

    task 3

---
+++ pagination/3-hypermedia_del_pagination.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+"""
+Deletion-resilient hypermedia pagination
+"""
+
+import csv
+import math
+from typing import List, Tuple, Dict
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+        self.__indexed_dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def indexed_dataset(self) -> Dict[int, List]:
+        """Dataset indexed by sorting position, starting at 0
+        """
+        if self.__indexed_dataset is None:
+            dataset = self.dataset()
+            truncated_dataset = dataset[:1000]
+            self.__indexed_dataset = {
+                i: dataset[i] for i in range(len(dataset))
+            }
+        return self.__indexed_dataset
+
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+        """This function returns a dict showing page info"""
+        if index is None:
+            index = 0
+        
+        assert index >=0, "index must be a positive integer"
+        assert isinstance(index, int), "Index must be integer type"
+        assert isinstance(page_size, int), "Page size must be integer type"
+
+        data = self.dataset()
+        page_data = data[index:index+ page_size]
+
+        return {
+            'index': index,
+            'next_index': index + page_size,
+            'page_size': page_size,
+            'data': page_data
+        }
