310d62977e07d90072b629ae9a8d9bb2d76c0530
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-08-31 12:25:19 +0000

    task 0:

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,4 +1,12 @@
 import ClassRoom from "./0-classroom.js";
 
-const room = new ClassRoom(10);
-console.log(room._maxStudentsSize)
+export default function initializeRooms(){
+    const ClassRoomArray = [];
+    const classRoomOne = new ClassRoom(19);
+    const classRoomTwo = new ClassRoom(20);
+    const classRoomThree = new ClassRoom(34);
+
+    ClassRoomArray.push(classRoomOne, classRoomTwo, classRoomThree);
+
+    return ClassRoomArray;
+};
