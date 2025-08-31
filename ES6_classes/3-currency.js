3d347e57fbff1daf545adf71d0fcfcf49f1dd600
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-08-31 12:58:52 +0000

    task 3

---
+++ ES6_classes/3-currency.js
@@ -0,0 +1,39 @@
+export default class Currency {
+    constructor(code, name) {
+    if (typeof code !=='string'){
+        throw new TypeError('Code must be a string');
+    }
+    if (typeof name !== 'string'){
+        throw new TypeError('Name must be a string');
+    }
+    this.code = code;
+    this.name = name;
+    }
+
+    get code(){
+        return this._code;
+    }
+    set code(code){
+        if((typeof code) === 'string'){
+            this._code = code;
+        } else {
+            throw new TypeError('Name must be a string');
+        }
+    }
+
+    get name() {
+        return this._name;
+    }
+    set name(name){
+        if((typeof name) === 'string'){
+            this._name = name;
+        } else {
+            throw new TypeError('Name must be a string');
+        }
+    }
+
+    displayFullCurrency() {
+        return `${this.name} (${this.code})`;
+    }
+
+}
