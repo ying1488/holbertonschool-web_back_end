a8cedfa393c58e4d8bbbe3ee1b7a4992845946c6
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-08-31 08:15:56 +0000

    task 8

---
+++ ES6_basic/10-loops.js
@@ -0,0 +1,11 @@
+export default function appendToEachArrayValue(array, appendString) {
+    const newArray = [];
+    let index = 0;
+
+  for (const value of array) {
+    newArray[index] = appendString + value;
+    index += 1;
+  }
+
+  return newArray;
+}
