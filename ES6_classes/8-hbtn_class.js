00bf5ca21eb0781f3c9f36dcfb6b05a6d22188d8
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-09-01 07:15:15 +0000

    task

---
+++ ES6_classes/8-hbtn_class.js
@@ -0,0 +1,29 @@
+export default class HolbertonClass {
+constructor(size, location){
+    if (typeof size !== 'number'){
+        throw new TypeError('size must be a number');
+        }
+    if (typeof location !== 'string'){
+        throw new TypeError('location must be a string');
+        }
+    
+    this._size = size;
+    this._location = location;
+    }
+
+    get size(){
+        return this._size;
+    }
+
+    get location(){
+        return this._location;
+    }
+
+    valueOf(){
+        return this._size;
+    }
+
+    toString() {
+        return this._location;
+    }
+};
