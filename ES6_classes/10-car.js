e5738710f3b78a0e7f21e0ddfc78241b4db0f9c6
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-09-03 01:02:07 +0000

    task 10

---
+++ ES6_classes/10-car.js
@@ -0,0 +1,20 @@
+export default class Car {
+constructor(brand, motor, color){
+    if (typeof brand !== 'string'){
+        throw new TypeError('brand must be a string');
+        }
+    if (typeof motor !== 'string'){
+        throw new TypeError('motor must be a string');
+        }
+    if (typeof color !== 'string'){
+        throw new TypeError('color must be a string');
+        }
+    
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+    }
+    cloneCar(){
+        return new this.constructor();
+    }
+};
