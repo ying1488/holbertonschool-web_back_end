253aba62a2815bac03b36f919941bc8d2e34f403
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-09-05 00:46:09 +0000

    task 0

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,9 +1,8 @@
-
-export default fuction getListStudents() {
-    const array = [
-        { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-        { id: 2, firstName: 'James', location: 'Columbia' },
-        { id: 5, firstName: 'Serena', location: 'San Francisco' }
-    ];
-    return array;
+export default function getLstStudents() {
+  const array = [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+  return array;
 }
