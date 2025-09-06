9730d72f4184119230d509e47db0500594789fc1
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-09-06 08:51:50 +0000

    task

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,14 @@
+import signUpUser from "./4-user-promise";
+import uploadPhoto from "./5-photo-reject";
+
+export default function handleProfileSignup(firstName, lastName, fileName){
+    return Promise.allSettled(
+        [signUpUser(firstName, lastName),
+            uploadPhoto(fileName),]
+    ).then ((result) => result.map((res)=>{
+        if (res.status === 'fulfilled'){
+            return {status: res.status, value: res.value};
+        }
+        return {status:res.status, value: res.reason.toString()};
+    }));
+}
