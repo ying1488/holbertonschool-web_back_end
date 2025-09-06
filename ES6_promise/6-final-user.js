a46d2a82bc2604f24427d3a03dbb5daea8183312
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-09-06 08:53:42 +0000

    task

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,5 +1,5 @@
-import signUpUser from "./4-user-promise";
-import uploadPhoto from "./5-photo-reject";
+import signUpUser from "./4-user-promise.js";
+import uploadPhoto from "./5-photo-reject.js";
 
 export default function handleProfileSignup(firstName, lastName, fileName){
     return Promise.allSettled(
