30344403e62b080dd520422488da7e4a44f7e501
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-09-07 06:13:17 +0000

    task

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,29 +1,29 @@
 const fs = require('fs');
 
 function countStudents(filePath) {
-    try {
-        const data = fs.readFileSync(filePath, 'utf8');
-        const lines = data.trim().split('\n');
-        const studentsCS = [];
-        const studentsSWE = [];
+  try {
+      const data = fs.readFileSync(filePath, 'utf8');
+      const lines = data.trim().split('\n');
+      const studentsCS = [];
+      const studentsSWE = [];
 
-        // Iterate through the lines and process student data
-        lines.forEach((line) => {
-            const [firstname, lastname, age, field] = line.split(',');
+      // Iterate through the lines and process student data
+      lines.forEach((line) => {
+          const [firstname, lastname, age, field] = line.split(',');
 
-            if (field === 'CS') {
-                studentsCS.push(firstname);
-            } else if (field === 'SWE') {
-                studentsSWE.push(firstname);
-            }
-        });
+          if (field === 'CS') {
+              studentsCS.push(firstname);
+          } else if (field === 'SWE') {
+              studentsSWE.push(firstname);
+          }
+      });
 
-        console.log(`Number of students: ${studentsCS.length + studentsSWE.length}`);
-        console.log(`Number of students in CS: ${studentsCS.length}. List: ${studentsCS.join(', ')}`);
-        console.log(`Number of students in SWE: ${studentsSWE.length}. List: ${studentsSWE.join(', ')}`);
-    } catch (error) {
-        throw new Error('Cannot load the database:');
-    }
+      console.log(`Number of students: ${studentsCS.length + studentsSWE.length}`);
+      console.log(`Number of students in CS: ${studentsCS.length}. List: ${studentsCS.join(', ')}`);
+      console.log(`Number of students in SWE: ${studentsSWE.length}. List: ${studentsSWE.join(', ')}`);
+  } catch (error) {
+      throw new Error('Cannot load the database:');
+  }
 }
 
 module.exports = countStudents;
