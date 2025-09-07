edb149bf3cd754b03aee0f29e93016b6a8e372eb
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-09-07 08:01:55 +0000

    task

---
+++ Node_JS_basic/7-http_express.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const countStudents = require('./3-read_file_async.js');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', async (req, res) => {
+  res.write('This is the list of our students\n');
+  try {
+    const data = await countStudents(process.argv[2]);
+    res.end(`${data.join('\n')}`)
+  } catch (err) {
+    console.error(err);
+    res.end('Cannot load the database')
+  }
+});
+
+app.listen(1245, () => {
+  console.log('Server is running on port 1245');
+});
+
+module.exports = app;
