413e092642cd5f5cffa0fe2170e8ffe134e0006a
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-09-07 07:39:34 +0000

    task 10

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,26 +1,26 @@
-const http = require('http');
-const countStudents = require('./3-read_file_async.js');
-const host = '127.0.0.1';
+var http = require('http');
+const countStudents = require('./3-read_file_async');
+const host = '127.0.0.1'
 
-const app = http.createServer(async (req, res) => {
+var app = http.createServer(async (req, res) => {
   const url = req.url;
 
-  if (url === '/'){
+  if (url === '/') {
     res.end('Hello Holberton School!');
   }
   if (url === '/students') {
-    res.write('This is the list of our students');
+    res.write('This is the list of our students\n');
     try {
-        const data = await countStudents(process,argv[2]);
-        res.end(`${data.join('\n')}`);
-    } catch (err){
-        console.error(err);
-        res.end('Cannot load the database')
+      const data = await countStudents(process.argv[2]);
+      res.end(`${data.join('\n')}`);
+    } catch (err) {
+      console.error(err);
+      res.end('Cannot load the database');
     }
-}
+  }
+});
+app.listen(1245, host, () => {
+  console.log('Server is running on http://127.0.0.1:1245');
 });
 
-app.listen(1245, host, ()=>{
-    console.log('Server is running on http://127.0.0.1:1245');
-});
 module.exports = app;
