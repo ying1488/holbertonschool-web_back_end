1ae9c699f9bb49f24d87509bf3ca1c5dab23e39a
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-09-07 07:30:34 +0000

    task 10

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,8 +1,8 @@
 const http = require('http');
 
-const app = http.createServer(function (req, res){
-    res.write('Hello Holberton School!');
-    res.end();
+const app = http.createServer((req, res) => {
+  res.write('Hello Holberton School!');
+  res.end();
 })
 
 app.listen(1245);
