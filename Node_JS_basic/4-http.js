c369db795949e9b1e9ef0ff17afbb5db23750da9
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-09-07 07:27:12 +0000

    task

---
+++ Node_JS_basic/4-http.js
@@ -0,0 +1,9 @@
+const http = require('http');
+
+const app = http.createServer(function (req, res){
+    res.write('Hello Holberton School!');
+    res.end();
+})
+
+app.listen(1245);
+module.exports = app;
