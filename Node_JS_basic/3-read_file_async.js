60ce3a8e217d732942317bbacc440092e27950b9
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-09-07 07:16:56 +0000

    task

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -2,29 +2,33 @@ const fs = require('fs').promises;
 
 async function countStudents(path) {
   try {
-    const data = fs.readFileSync(filePath, 'utf8');
+    const data = await fs.readFile(path, { encoding: 'utf-8' });
     const lines = data.split('\n').filter((line) => line.trim() !== '');
-    const rows = lines.slice(1);
+    const rows = lines.slice(1); // Exclude the header
 
     const totalStudents = rows.length;
     const studentsCS = [];
     const studentsSWE = [];
 
     for (const row of rows) {
-        const columns = row.split(',');
-        const studentName = columns[0];
-        const field = columns[3];
-        if (field === 'CS') studentsCS.push(studentName);
-        if (field === 'SWE') studentsSWE.push(studentName);
+      const columns = row.split(',');
+      const studentName = columns[0];
+      const field = columns[3];
+
+      if (field === 'CS') studentsCS.push(studentName);
+      if (field === 'SWE') studentsSWE.push(studentName);
     }
+
+    // Construct array with formatted lines
     const output = [
-        `Number of students: ${totalStudents}`,
-        `Number of students in CS: ${studentsCS.length}. List: ${studentsCS.join(', ')}`,
-        `Number of students in SWE: ${studentsSWE.length}. List: ${studentsSWE.join(', ')}`
+      `Number of students: ${totalStudents}`,
+      `Number of students in CS: ${studentsCS.length}. List: ${studentsCS.join(', ')}`,
+      `Number of students in SWE: ${studentsSWE.length}. List: ${studentsSWE.join(', ')}`,
     ];
+
     return output;
-    } catch (error){
-    throw new Error('Cannot load the database:');
+  } catch (error) {
+    throw new Error('Cannot load the database');
   }
 }
 
