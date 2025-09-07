7e26524a0bdf5ad9692b8e37c3195208ad6cf9ab
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-09-07 05:03:29 +0000

    task

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,13 @@
+process.stdin.setEncoding('utf-8');
+
+process.stdout.write('Welcome to Holberton School, what is your name?\n')
+
+process.stdin.on('data',(data) => {
+  process.stdout.write(
+    `Your name is: ${data}`
+  );
+});
+
+process.stdin.on('end',() => {
+    process.stdout.write('This is important software is now closing\n');
+});
