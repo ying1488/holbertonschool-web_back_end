e7721fe4e6a0a481722b2470e6c3cf5379629a13
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-09-07 07:37:20 +0000

    task 10

---
+++ Node_JS_basic/5-http.js
@@ -0,0 +1,25 @@
+const http = require('http');
+const countStudents = require('./3-read_file_async.js');
+const host = '127.0.0.1';
+
+const app = http.createServer((req, res) => {
+  const url = req.url;
+
+  if (url === '/'{
+    res.end('Hello Holberton School!');
+  }
+  if (url === '/students') {
+    res.write('This is the list of our students');
+    try {
+        const data = await countStudents(process,argv[2]);
+        res.end(`${data.join('\n')}`);
+    } catch (err){
+        console.error(err);
+        res.end('Cannot load the database')
+    }
+});
+
+app.listen(1245, host, ()=>{
+    console.log('Server is running on http://127.0.0.1:1245');
+});
+module.exports = app;
